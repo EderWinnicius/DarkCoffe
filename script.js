1e5fc83035058ac488ec33aa78f8fc4a9c43132d
Eder Oliviera <eder-winnicius@hotmail.com>
2024-06-14 17:06:17 +0000

    Adicionando Js e Botões

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// Add JS here
+function toggleCheckbox(checkboxElement) {
+    checkboxElement.classList.toggle('checked');
+  }
+
+
+  function scrollbtn(){
+    window.scrollTo({ top: 10000, behavior: 'smooth' });
+
+    var btnArrow = document.getElementById('arrowbt');
+    btnArrow.classList.toggle('rowbtn');
+
+    const scrollPosition = window.pageYOffset;
+    if (scrollPosition > 100) {
+    
+        window.scrollTo({ top: -10000, behavior: 'smooth' });
+
+  }
+}
